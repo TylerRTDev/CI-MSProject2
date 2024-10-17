a596ca79f91f0d032e4631472c9499c8dea7032f
TylerRTDev <tylerrtdev@outlook.com>
2024-10-17 16:51:40 +0100

    Add JS file and logic

---
+++ script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.pattern-button');
+const startButton = document.getElementById('start-game');
+const messageDisplay = document.getElementById('message');
+const scoreDisplay = document.getElementById('score');
+
+let gamePattern = [];
+let userPattern = [];
+let score = 0;
